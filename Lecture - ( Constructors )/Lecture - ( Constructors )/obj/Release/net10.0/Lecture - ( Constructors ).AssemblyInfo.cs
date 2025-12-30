689c8590edaeeb37758b340e71d23a0b200fa07e
CodeSmithMayank <mayankpal4812@gmail.com>
2025-12-30 21:07:14 +0530

    Lecture - ( Abstraction )

--- Lecture - ( Constructors )/Lecture - ( Constructors )/obj/Release/net10.0/Lecture - ( Constructors ).AssemblyInfo.cs
+++ Lecture - ( Constructors )/Lecture - ( Constructors )/obj/Release/net10.0/Lecture - ( Constructors ).AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lecture - ( Constructors )")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a172d25326d0a030979ad086bceb8a278772b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lecture - ( Constructors )")]
